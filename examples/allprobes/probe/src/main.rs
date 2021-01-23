2ea12dce199ae17509f81c1c895b1818ea000076
David Craven <david@craven.ch>
2021-01-23 23:08:40 +0100

    Add tests for other probes.

--- examples/allprobes/probe/src/main.rs
+++ examples/allprobes/probe/src/main.rs
@@ -6,25 +6,76 @@ use bpf_helpers::{entry, map, program, HashMap, U32};
 program!(0xFFFF_FFFE, b"GPL");
 
 #[map]
-static PROBE_COUNT: HashMap<U32, U32> = HashMap::with_max_entries(10);
+static PROBE_COUNT: HashMap<U32, U32> = HashMap::with_max_entries(13);
 
-#[entry("kprobe")]
-fn kprobe(_args: &pt_regs) {
+#[inline(always)]
+fn increase_counter(n: u32) {
     PROBE_COUNT.get_or_default(&U32::new(0), |count| {
         count.set(count.get() + 1);
     });
 }
 
 #[entry("kprobe")]
+fn kprobe(_args: &pt_regs) {
+    increase_counter(0)
+}
+
+#[entry("kprobe")]
 fn kretprobe(_args: &pt_regs) {
-    PROBE_COUNT.get_or_default(&U32::new(1), |count| {
-        count.set(count.get() + 1);
-    });
+    increase_counter(1)
+}
+
+#[entry("kprobe")]
+fn uprobe(_args: &pt_regs) {
+    increase_counter(2)
+}
+
+#[entry("kprobe")]
+fn uretprobe(_args: &pt_regs) {
+    increase_counter(3)
+}
+
+#[entry("kprobe")]
+fn usdt(_args: &pt_regs) {
+    increase_counter(4)
 }
 
 #[entry("raw_syscalls:sys_enter")]
 fn tracepoint(_args: &SysEnter) {
-    PROBE_COUNT.get_or_default(&U32::new(2), |count| {
-        count.set(count.get() + 1);
-    });
+    increase_counter(5)
 }
+
+#[entry("perf_event")]
+fn profile(_args: &bpf_perf_event_data) {
+    increase_counter(6)
+}
+
+#[entry("perf_event")]
+fn interval(_args: &bpf_perf_event_data) {
+    increase_counter(7)
+}
+
+#[entry("perf_event")]
+fn software(_args: &bpf_perf_event_data) {
+    increase_counter(8)
+}
+
+#[entry("perf_event")]
+fn hardware(_args: &bpf_perf_event_data) {
+    increase_counter(9)
+}
+
+#[entry("perf_event")]
+fn watchpoint(_args: &bpf_perf_event_data) {
+    increase_counter(10)
+}
+
+/*#[entry("tracing")]
+fn kfunc(_args: &core::ffi::c_void) {
+    increase_counter(11)
+}*/
+
+/*#[entry("tracing")]
+fn kretfunc(_args: &core::ffi::c_void) {
+    increase_counter(12)
+}*/
--- examples/allprobes/src/main.rs
+++ examples/allprobes/src/main.rs
@@ -7,13 +7,37 @@ static PROBE: &[u8] = include_bytes!(concat!(
     "/target/bpf/programs/allprobes-probe/allprobes-probe.elf",
 ));
 
-static PROBES: &[&str] = &["kprobe", "kretprobe", "tracepoint"];
+static PROBES: &[&str] = &[
+    "kprobe",
+    "kretprobe",
+    "uprobe",
+    "uretprobe",
+    "usdt",
+    "tracepoint",
+    "profile",
+    "interval",
+    "software",
+    "hardware",
+    "watchpoint",
+    "kfunc",
+    "kretfunc",
+];
 
 fn main() -> Result<()> {
     let mut bpf = BpfBuilder::new(PROBE)?
         .attach_probe("kprobe:finish_task_switch", "kprobe")?
         .attach_probe("kretprobe:finish_task_switch", "kretprobe")?
+        //.attach_probe("uprobe:/path:symbol")?
+        //.attach_probe("uretprobe:/path:symbol")?
+        //.attach_probe("usdt:/path:probe")?
         .attach_probe("tracepoint:raw_syscalls:sys_enter", "tracepoint")?
+        .attach_probe("profile:hz:99", "profile")?
+        //.attach_probe("interval:ms:100", "interval")?
+        //.attach_probe("software:event:count", "software")?
+        //.attach_probe("hardware:event:count", "hardware")?
+        //.attach_probe("watchpoint:address:length:mode", "watchpoint")?
+        //.attach_probe("kfunc:func", "kfunc")?
+        //.attach_probe("kretfunc:func", "kretfunc")?
         .load()?;
     let map = bpf.hash_map::<U32, U32>("PROBE_COUNT")?;
     std::thread::sleep(Duration::from_millis(1000));
